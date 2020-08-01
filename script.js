0ee2bf958287a812e1fad4cf8b59a60c3723f24f
gabdykalyk <gabdykalyk@gmail.com>
2020-08-01 22:02:31 +0600

    responce fixed

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //TASK 1
 
+let xhr = new XMLHttpRequest()
+let URL = 'https://jsonplaceholder.typicode.com/photos?_start=0&_end=30';
+xhr.open('GET', URL)
+xhr.send()
+
 let body = document.querySelector('body')
 function textUpdate() {
-    let xhr = new XMLHttpRequest()
-    let URL = 'https://jsonplaceholder.typicode.com/posts';
-    xhr.open('GET', URL)
-    xhr.send()
-
     xhr.addEventListener('load', () => {
         let data = JSON.parse(xhr.response)
         data.forEach(element => {
@@ -29,11 +29,6 @@ textUpdate()
 
 // TASK 2
 function image() {
-    let xhr = new XMLHttpRequest()
-    let URL = 'http://jsonplaceholder.typicode.com/photos?_start=0&_end=30';
-    xhr.open('GET', URL)
-    xhr.send()
-
     xhr.addEventListener('load', () => {
         let data = JSON.parse(xhr.response)
         let carousel = document.querySelector('#carousel')
@@ -51,17 +46,7 @@ function image() {
                     </div>`
                 div.classList.add('carousel-item')
                 carousel.append(div)
-            }
-
-            // data.forEach(element => {
-            //     let div = document.createElement('div')
-            //     div.innerHTML = `<img src="${element.url}" class="d-block w-100" alt="...">
-            //         <div class="carousel-caption d-none d-md-block">
-            //             <h5>${element.title}</h5>
-            //         </div>`
-            //     div.classList.add('carousel-item')
-            //     carousel.append(div)
-            // });      
+            }    
         }
 
         renderDiv()
